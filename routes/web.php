a3e6eba4c608b037469dee9d13638db48970f3d8
gilardodestri <gilardodestri@gmail.com>
2025-02-01 10:20:37 +0700

    Controller test

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestController;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,9 +19,9 @@ Route::get('/', [TestController::class, 'index']);
 Route::post('/test', [TestController::class, 'store'])->name('test.store');
 Route::get('/test/result/{user}', [TestController::class, 'result'])->name('test.result');
 
-Route::get('admin', [TestController::class, 'indexadmin'])->name('admin.index');
-Route::get('admin/create', [TestController::class, 'create'])->name('admin.create');
-Route::post('admin', [TestController::class, 'simpansoaladmin'])->name('admin.store');
-Route::get('admin/{question}/edit', [TestController::class, 'edit'])->name('admin.edit');
-Route::put('admin/{question}', [TestController::class, 'update'])->name('admin.update');
-Route::delete('admin/{question}', [TestController::class, 'destroy'])->name('admin.destroy');
+Route::get('admin', [AdminController::class, 'indexadmin'])->name('admin.index');
+Route::get('admin/create', [AdminController::class, 'create'])->name('admin.create');
+Route::post('admin', [AdminController::class, 'simpansoaladmin'])->name('admin.store');
+Route::get('admin/{question}/edit', [AdminController::class, 'edit'])->name('admin.edit');
+Route::put('admin/{question}', [AdminController::class, 'update'])->name('admin.update');
+Route::delete('admin/{question}', [AdminController::class, 'destroy'])->name('admin.destroy');
